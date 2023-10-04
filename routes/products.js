b42fd049cae6959352f8e79f97a341dc5e6bff2f
gmsm93 <116608456+gmsm93@users.noreply.github.com>
2023-10-03 21:20:24 -0500

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
+
 router.get('/', (req, res) => {
   fs.readFile('productos.json', 'utf8', (err, data) => {
     if (err) {
